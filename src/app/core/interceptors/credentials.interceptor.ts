14cceb5b20e270a26752c7c172cb8eb842603203
mohitupa <mohitu531@gmail.com>
2026-06-23 19:22:40 +0530

    interceptor changes

--- src/app/core/interceptors/credentials.interceptor.ts
+++ src/app/core/interceptors/credentials.interceptor.ts
@@ -1,8 +1,18 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
-export const credentialsInterceptor:
-HttpInterceptorFn =
+export const credentialsInterceptor: HttpInterceptorFn =
 (req, next) => {
+
+  const requiresCredentials =
+    req.url.includes('/auth/') ||
+    req.url.includes('/dashboard/') ||
+    req.url.includes('/admin-users/') ||
+    req.url.includes('/media/') ||
+    req.url.includes('/contact-messages/');
+
+  if (!requiresCredentials) {
+    return next(req);
+  }
 
   return next(
     req.clone({
